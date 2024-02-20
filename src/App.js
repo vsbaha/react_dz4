959834438a7d8cfe2b7e4c70574dca30d594cc28
vsbaha <azimkanovbaktiar@gmail.com>
2024-02-20 19:40:58 +0600

    dz4 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const RegistrationForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [mail, setMail] = useState("")
+  const [submittedData, setSubmittedData] = useState(null);
+
+  const handleMailChange = (e) => {
+    setMail(e.target.value);
+  };
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const regExp = /^\w+@gmail\.com$/
+    if (regExp.test(mail)){
+      const htmlContent = (
+        <ul>
+          <p>e-mail: {mail}</p>
+          <p>имя: {username}</p>
+          <p>пароль: {password}</p>
+        </ul>
+      );
+      setSubmittedData(htmlContent);
+    } else {
+      alert ('bad')
+    }
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit} className="registration-form">
+        <h3>Регистрация</h3>
+        <input
+          type="text"
+          value={mail}
+          onChange={handleMailChange}
+          placeholder="e-mail"
+          className="form-input"
+          required
+        />
+        <input
+          type="text"
+          value={username}
+          onChange={handleUsernameChange}
+          placeholder="Username"
+          className="form-input"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          placeholder="Password"
+          className="form-input"
+          required
+        />
+        <button type="submit" className="form-button">
+          Регистрация
+        </button>
+        {submittedData}
+      </form>
+    </>
   );
-}
+};
 
-export default App;
+export default RegistrationForm;
